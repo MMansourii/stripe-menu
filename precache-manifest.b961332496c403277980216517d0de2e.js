636914461686af068b83b50a0f796c898ded0e14
Mohammad <57011800+Mammad10@users.noreply.github.com>
2021-07-07 14:19:54 +0300

    Updates

---
+++ precache-manifest.b961332496c403277980216517d0de2e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6eb9f6afdf86a1abe03e1c9a99a56d50",
+    "url": "/github.io/stripe/menu/index.html"
+  },
+  {
+    "revision": "98f98c5f5e4f9692268a",
+    "url": "/github.io/stripe/menu/static/css/main.adc7fc53.chunk.css"
+  },
+  {
+    "revision": "9d9ce4078b442a71cc60",
+    "url": "/github.io/stripe/menu/static/js/2.e1e77988.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.io/stripe/menu/static/js/2.e1e77988.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98f98c5f5e4f9692268a",
+    "url": "/github.io/stripe/menu/static/js/main.6942afa1.chunk.js"
+  },
+  {
+    "revision": "4acca8c968cc30cdff99",
+    "url": "/github.io/stripe/menu/static/js/runtime-main.86e03eff.js"
+  },
+  {
+    "revision": "882a211ad0bae36b9c11715c9291f36b",
+    "url": "/github.io/stripe/menu/static/media/hero.882a211a.svg"
+  },
+  {
+    "revision": "86d618b5be1508bcbbb31d6393f089f6",
+    "url": "/github.io/stripe/menu/static/media/logo.86d618b5.svg"
+  },
+  {
+    "revision": "e908706c41907a9611ba9c4d61a32f7c",
+    "url": "/github.io/stripe/menu/static/media/phone.e908706c.svg"
+  }
+]);
